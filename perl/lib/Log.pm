97cdac11dc944b752a89f2d4d3846a2b303cb19b
e-mon <emon18@icloud.com>
2015-07-28 00:53:43 +0900

    implement to_hash function in Log class for test

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -4,36 +4,54 @@ use warnings;
 use DateTime;
 
 sub new {
-    my ($class, %args) = @_;
+    my ( $class, %args ) = @_;
     return bless \%args, $class;
 }
 
 sub protocol {
-   my $self = shift;
-   my @req = split(/ /,$self->{req});
-   return $req[2];
+    my $self = shift;
+    my @req = split( ' ', $self->{req} );
+
+    return $req[2];
 }
 
 sub method {
-   my $self = shift;
-   my @req = split(/ /,$self->{req});
-   return $req[0];
+    my $self = shift;
+    my @req = split( ' ', $self->{req} );
+
+    return $req[0];
 }
 
 sub path {
-   my $self = shift;
-   my @req = split(/ /,$self->{req});
-   return $req[1];
+    my $self = shift;
+    my @req = split( ' ', $self->{req} );
+
+    return $req[1];
 }
 
 sub uri {
-   my $self = shift;
-   return "http://".$self->{host}.$self->path;
+    my $self = shift;
+    return "http://" . $self->{host} . $self->path;
 }
 
 sub time {
-   my $self = shift;
-   return DateTime->from_epoch(epoch=>$self->{epoch});
+    my $self = shift;
+    return DateTime->from_epoch( epoch => $self->{epoch} );
+}
+
+sub to_hash {
+    my $self   = shift;
+    my $params = {
+        'method' => $self->method,
+        'time'   => $self->time,
+        'uri'    => $self->uri
+    };
+    my $ret = { %{$self}, %{$params} };
+    delete $ret->{epoch};
+    delete $ret->{host};
+    delete $ret->{req};
+
+    return $ret;
 }
 
 1;
