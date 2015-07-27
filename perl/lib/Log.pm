e27fef017e80081fad3e87cf9a5f55ff12941f0b
e-mon <emon18@icloud.com>
2015-07-27 18:00:06 +0900

    implement Log.pm

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,6 +1,7 @@
 package Log;
 use strict;
 use warnings;
+use DateTime;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,18 +9,31 @@ sub new {
 }
 
 sub protocol {
+   my $self = shift;
+   my @req = split(/ /,$self->{req});
+   return $req[2];
 }
 
 sub method {
+   my $self = shift;
+   my @req = split(/ /,$self->{req});
+   return $req[0];
 }
 
 sub path {
+   my $self = shift;
+   my @req = split(/ /,$self->{req});
+   return $req[1];
 }
 
 sub uri {
+   my $self = shift;
+   return "http://".$self->{host}.$self->path;
 }
 
 sub time {
+   my $self = shift;
+   return DateTime->from_epoch(epoch=>$self->{epoch});
 }
 
 1;
