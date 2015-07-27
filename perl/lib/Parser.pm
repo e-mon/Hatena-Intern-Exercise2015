d567ee5de5c4c738533d208e5b763c63e700590e
e-mon <emon18@icloud.com>
2015-07-27 23:36:34 +0900

    implement Parser.pm

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -1,13 +1,31 @@
 package Parser;
 use strict;
 use warnings;
+use Log;
 
 sub new {
-    my ($class, %args) = @_;
+    my ( $class, %args ) = @_;
     return bless \%args, $class;
 }
 
 sub parse {
+    my ( $self, $line ) = @_;
+    open my $fh, '<', $self->{filename} or die $!;
+    return [ map { $self->_parser($_) } <$fh> ];
+}
+
+sub _parser {
+    my $line   = $_;
+    my %params = ( $line =~ /(\w+):(.+?)[\t|\n]/g );
+    my %log;
+
+    foreach my $key ( keys %params ) {
+        if ( $params{$key} ne "-" ) {
+            $log{$key} = $params{$key};
+        }
+    }
+
+    return Log->new(%log);
 }
 
 1;
