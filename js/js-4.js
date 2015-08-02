b03929d5abbab8461e4dcfc27626d5e8e26c843e
e-mon <emon18@icloud.com>
2015-08-02 20:23:07 +0900

    implement incremental search method

---
+++ js/js-4.js
@@ -0,0 +1,31 @@
+function search(){
+   var elem = document.getElementById('table-container');
+   var text = document.getElementById('log-input').value;
+   var key = document.getElementById('column').value;
+   var value = document.getElementById('search-value').value;
+   
+   elem.innerHTML = "";
+   logs = parseLTSVLog(text).filter(function(log){
+       if (key === "" || value === ""){
+           return true;
+       }else{
+           return filter(key, value, log);
+       }
+   });
+   createLogTable(elem, logs);
+}
+
+function filter(key, value, log){
+    if(!isNaN(log[key])){
+        text = String(log[key]);
+    }else{
+        text = log[key];
+    }
+    if(~text.indexOf(value)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+document.getElementById('search-value').addEventListener("keyup",search,false);
