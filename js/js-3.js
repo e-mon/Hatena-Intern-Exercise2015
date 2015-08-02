fe66b5f6b160eef89bb042187285a89a383d1bce
e-mon <emon18@icloud.com>
2015-08-02 12:24:05 +0900

    implement function to display log table

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,10 @@
 // 課題 JS-3 の実装をここに記述してください。
+function displayLogTable(){
+   var elem = document.getElementById('table-container');
+   var text = document.getElementById('log-input').value;
+   
+   elem.innerHTML = "";
+   createLogTable(elem, parseLTSVLog(text));
+}
+
+document.getElementById('submit-button').addEventListener("click",displayLogTable,false);
